68095860200f3d17f7e3ca8a7486e121bf22f8c1
Andrew Bunton <ajbunton@gmail.com>
2014-03-26 16:35:22 -0500

    Remove unneeded code & files, fix cancel button

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -15,10 +15,33 @@ ready = function() {
 		$('#new_list_form').show();
 	});
 
+  $('#new_task_link').on('click', function(e){
+    e.preventDefault();
+    $('#add_task_form').show();
+  });
+
+  $('#edit_list_name').on('click', function(e){
+    e.preventDefault();
+    $('#update_list_form').show();
+    $('#edit_list_name').hide();
+  })
+
   $('#cancel').on('click', function(e){
     e.preventDefault();
-    $('#edit_list_name').hide();
+    $('#update_list_form').hide();
+    $('#list_name').val('');
+    $('#edit_list_name').show();
+    $('#new_list_form').hide();
+    $('#new_list').show();
+    $('#list_name').val('');
   });
+
+  $('#cancel_task').on('click', function(e){
+    e.preventDefault();
+    $('#add_task_form').hide();
+    $('#task_desc').val('');
+    $('#new_task_link').show();
+  })
 
 };
 
